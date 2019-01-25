0287d09f32aeb88b2b5cab36cbe2398a663ab582
ViiRo <45905915+ViiRo@users.noreply.github.com>
2019-01-25 08:20:58 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if(message.content === prefix+'مبلغ'){
-        message.channel.send('#daily')
+        message.channel.send('#credit')
     }
 });
 
